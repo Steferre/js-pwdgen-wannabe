6372c3bbcbd44998d3dffdb4e8207ef3ba78280b
Steferre <ferrera.ste@gmail.com>
2021-04-08 09:46:08 +0200

    Esercizio fatto secondo consegna.

---
+++ script.js
@@ -0,0 +1,9 @@
+var yourName = prompt("What's your name?");
+
+var yourSurname = prompt("What's your surname?");
+
+var yourFavColor = prompt("What is your favourite color?");
+
+document.getElementById('userPsw').innerHTML= (yourName + yourSurname + yourFavColor + "21");
+
+console.log(yourName + yourSurname + yourFavColor + "21");
